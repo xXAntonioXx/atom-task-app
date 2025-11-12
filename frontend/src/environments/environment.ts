2ae00219120625fe875b2f04c99f0f070e828a51
Antonio Gonzalez <jose_antony11@hotmail.com>
2025-11-12 09:57:24 -0700

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,13 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
-    firebase: {
-        apiKey: 'AIzaSyD4po_cYMsIfltvDcbKaDIUKL6OMhiIfEg',
-        authDomain: 'atom-tasks-app.firebaseapp.com',
-        projectId: 'atom-tasks-app',
-        storageBucket: 'atom-tasks-app.firebasestorage.app',
-        messagingSenderId: '137849172975',
-        appId: '1:137849172975:web:f53358126dbc503a557c1a',
-        measurementId: 'G-MC4270SLHR',
-    },
+    apiUrl: '',
+    firebase: {},
 };
